d12733934f09c4c870b35f9afcee74360d9662ac
lemondy97 <104114377+lemondy97@users.noreply.github.com>
2022-06-15 09:14:13 +0900

    20220615_project2_jingyu1
    미디어쿼리 업데이트

--- js/Nav_mobile.js
+++ js/Nav_mobile.js
@@ -2,11 +2,9 @@
                         $('.menu_icon').click(function(){
                             $('#reveal_menu').css('opacity', '1');
                             $('#reveal_menu').css('width', '100%');
-                            $('body').not('#reveal_menu').addClass('hide')
                         });
                         $('.disappear_menu').click(function(){
                             $('#reveal_menu').css('opacity', '0');
                             $('#reveal_menu').css('width', '0%');
-                            $('body').removeClass('hide');
                         });
                     });
